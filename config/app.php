079d2bab904c29c7a9972be68e723e5197184000
zhang-jim <135114067+zhang-jim@users.noreply.github.com>
2023-12-04 14:12:45 +0800

    laravel,Breeze,Vue 安裝

--- config/app.php
+++ config/app.php
@@ -70,7 +70,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'Asia/Taipei',
 
     /*
     |--------------------------------------------------------------------------
@@ -168,8 +168,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\FortifyServiceProvider::class,
-        App\Providers\JetstreamServiceProvider::class,
     ])->toArray(),
 
     /*
